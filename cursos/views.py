53f3dfbb28008fa8c6fb0173fa32ccce68847451
BrunoCamargoDev <bcamargo414@gmail.com>
2025-08-18 20:42:24 -0300

    Atualizações

--- cursos/views.py
+++ cursos/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
 from django.contrib import messages
+from django.views.generic import TemplateView
 
 class IndexView(ListView):
     model = Curso
@@ -22,16 +23,48 @@ class IndexView(ListView):
             qs= Curso.objects.all()
 
         titulo = self.request.GET.get('titulo')
-
         if titulo:
             qs = qs.filter(titulo__icontains=titulo)
 
-        return qs.order_by('titulo').select_related('categoria').prefetch_related('')
+        return qs.order_by('titulo').select_related('categoria').prefetch_related('instrutores')
+    
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['titulo_pagina'] = (
+            'Meus cursos' if self.request.user.is_authenticated else 'Cursos Disponíveis'
+        )
+        return ctx
+
+class TodosCursosView(ListView):
+    model = Curso
+    template_name = 'index.html'
+    context_object_name = 'cursos'
+
+    def get_queryset(self):
+        qs = Curso.objects.all().order_by('titulo')
+        titulo = self.request.GET.get('titulo')
+        if titulo :
+            qs = qs.filter(titulo__icontains=titulo)
+        return qs
+    
+    def get_context_data(self, **kwargs):
+        curso = super().get_context_data(**kwargs)
+        curso['titulo_pagina'] = 'Todos os Cursos'
+        return curso
 
 class CursoDetailView(DetailView):
     model = Curso
     template_name = 'curso_detail.html'
     context_object_name = 'curso'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self.request.user
+        ctx['ja_inscrito'] = {
+            user.is_authenticated
+            and Inscricao.objects.filter(usuario=user, curso=self.object).exists()
+        }
+        return ctx
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class InscreverCursoView(ListView):
@@ -49,3 +82,15 @@ class InscreverCursoView(ListView):
             messages.warning(request, "Você já está inscrito nesse curso!")
 
         return redirect('curso_detail', pk=curso_id)
+    
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class ProjetosView(TemplateView):
+    template_name = "projetos.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["projetos"] = [
+            {"nome": "Projeto cursos",
+                    "link": "https://github.com/BrunoCamargoDev/Projeto-Django-Cursos.git"}
+        ]
+        return ctx
